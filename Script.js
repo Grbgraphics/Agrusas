2c3519d9c86143a8b9bc6e89f1c9fb154fbfa75e
Grbgraphics <90997222+Grbgraphics@users.noreply.github.com>
2023-04-18 19:13:21 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,5 +2,9 @@ const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
 
 burger.addEventListener('click', () => {
-	menu.classList.toggle('menu-active');
+    // Toggle the "menu-active" class on the menu
+    menu.classList.toggle('menu-active');
+    
+    // Toggle the "active" class on the burger icon
+    burger.classList.toggle('active');
 });
